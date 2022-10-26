f8f3386a2488cdfba9252b3cdfbcd01cc63eac44
mitHSwas <wdev.mithunbiswas@gmail.com>
2022-10-26 13:26:50 +0600

    category wise course data is updated

--- index.js
+++ index.js
@@ -6,8 +6,14 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+const categories = require("./data/categories.json");
+
 app.get("/", (req, res) => {
-    res.send("Progress hub is generating wet bro...")
+    res.send("Progress hub is generating wait bro...")
+})
+
+app.get("/category", (req, res) => {
+    res.send(categories);
 })
 
 app.listen(port, () => {
