9c288c6605a11e1b16173add52a62f4632805d03
mitHSwas <wdev.mithunbiswas@gmail.com>
2022-10-26 20:36:07 +0600

    category wise dynamic route is created for cetegory course details

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require("./data/categories.json");
+const courses = require("./data/courseDetails.json");
 
 app.get("/", (req, res) => {
     res.send("Progress hub is generating wait bro...")
@@ -14,6 +15,15 @@ app.get("/", (req, res) => {
 
 app.get("/category", (req, res) => {
     res.send(categories);
+})
+app.get("/category/:id", (req, res) => {
+    const id = req.params.id;
+    const course = courses.filter(course => course.category_id === id)
+    res.send(course);
+})
+
+app.get("/courses", (req, res) => {
+    res.send(courses)
 })
 
 app.listen(port, () => {
